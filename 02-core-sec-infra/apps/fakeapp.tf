325d0206b285b489f127a9fe7bc06abdb772b6cf
sebbycorp <sebastian@maniak.io>
2023-11-10 12:27:35 -0500

    vars

--- 02-core-sec-infra/apps/fakeapp.tf
+++ 02-core-sec-infra/apps/fakeapp.tf
@@ -1,10 +1,3 @@
-
-variable "fwsshkey" {
-  description = "The fwsshkey where the EC2 instance will be deployed"
-}
-variable "customer_vpc_id" {
-  description = "The customer_vpc_id where the EC2 instance will be deployed"
-}
 variable "csprivatesubnetaz2" {
 
 }
